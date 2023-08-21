1fb7d1ef52ecb577ba640acf2e319f5a45fd896e
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-08-21 11:38:26 +0300

    Created lucky game

---
+++ Lucky_Game/README.md
@@ -0,0 +1,22 @@
+# LUCKY NUMBER GAME
+
+- This game allows user to make guesses for the lucky number
+- The user only has three trials after which, the end incase of failed attempt
+
+## To compile the file code to executable file use the following command in your terminal:
+
+```
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o Luckygame
+```
+
+#### To play the game, run this script in your terminal after compilation
+
+```
+./Luckygame
+```
+
+### ENJOY THE GAME
+
+Developed by [Collins](https://github.com/Collins331)
+
+Follow for more projects and give a star.
---
+++ Lucky_Game/luckygame.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/*
+ *luckygame: the entry point to the game
+ *Description: doesn't receive any argument
+ *Return: 1(success) else -1(failed)
+ */
+int luckygame(void)
+{
+    int num;
+    int lucky_num = 8;
+    int attempts = 3;
+    int trial = 1;
+    /*Welcomes the user and instructs them on how to play*/
+    printf("Welcome to lucky number guess game\n");
+    sleep(1);
+    printf("You have 3 attempts to guess the number\n");
+
+    while (trial <= 3)
+    {
+        printf("Please enter your guess for lucky number\n");
+        /*Prompts the user to give a guess of the lucky number*/
+        scanf("%d", &num);
+
+        if (num == lucky_num)
+        {
+            printf("Congratulations you won, The lucky number is 8\n\n");
+            /*Congratulates the user, and exits the game*/
+            return (1);
+        }
+        else
+        {
+            /*If the user fails the guess, a prompt is displayed.*/
+            printf("Oopps!! your answer was wrong, please try again\n");
+            sleep(1);
+            printf("You have %d attempts left\n\n", attempts - trial);
+            if ((attempts - trial) == 0)
+            {
+                printf("You lost!!\nStart a new game.\n\n");
+                return (-1);
+            }
+        }
+        trial++;
+    }
+}
+/*
+ *main - entry point to the application
+ *Return: always 0
+ */
+int main(void)
+{
+    int num;
+
+    luckygame();
+    /*This is an infinite loop incase the user wants to continue playing the game*/
+    while (1)
+    {
+        printf("Do you want to start new game?\nSelect an option below\n\n");
+        printf("1. Start new game\n2. Exit the game\n\n");
+        scanf("%d", &num);
+
+        if (num == 1)
+        {
+            printf("Loading...\n\n");
+            sleep(3);
+            luckygame();
+        }
+        else if (num == 2)
+        {
+            printf("Exiting...\n");
+            sleep(2);
+            printf("Thank you for playing the game\n");
+            sleep(2);
+            printf("Goodbyee...\n");
+            exit(1);
+        }
+        else
+        {
+            printf("You entered invalid option\nPlease try again...");
+            sleep(2);
+        }
+    }
+
+    return (0);
+}
